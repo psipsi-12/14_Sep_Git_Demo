c827ecdd72d6d65d0698582da84026176f5ddc17
adi <adi@gmail.com>
2025-01-29 14:15:36 +0530

    second commit

--- src/test/java/Demo/Test1.java
+++ src/test/java/Demo/Test1.java
@@ -5,6 +5,11 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("hellow");
         System.out.println("hi");
+        System.out.println("hiii"); 
+        System.out.println("hhjvhhj"); 
+	}
+	public void name() {
+		System.out.println("jgng");
 	}
 
 }
