4abda3a81c463c2e4a9d70d608e2de73c699d0a4
adi <adi@gmail.com>
2025-01-29 14:28:29 +0530

    3 commit

---
+++ src/test/java/Demo/ifelseifladder4.java
@@ -0,0 +1,28 @@
+package Demo;
+
+public class ifelseifladder4 {
+
+	public static void main(String[] args) {
+		
+		int x =20;
+		int y =40;
+		int z=100;
+		
+		if (x>y && x>z) {
+			System.out.println("x is greter number");
+		}
+		
+		else if (y>x && y>z) {
+			System.out.println("y is greter number");
+		}
+		
+		else  {
+			System.out.println("z is greter number");
+		}
+		
+	
+		
+
+	}
+
+}
---
+++ src/test/java/Demo/ifelseifladder5.java
@@ -0,0 +1,25 @@
+package Demo;
+
+public class ifelseifladder5 {
+
+	public static void main(String[] args) {
+	   
+		int number = 12;
+		
+		if (number >0) {
+			
+			System.out.println("number is positive");
+			
+		}
+		
+		else if (number <0) {
+			System.out.println("number is negative");
+		}
+		
+		else {
+			System.out.println("number is zero");
+		}
+
+	}
+
+}
